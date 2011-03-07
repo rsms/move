9e117dcfb4e130a71c1395ae201ac90f907a900a
Holtwick <dirk.holtwick@gmail.com>
2011-03-07 08:54:29 +0100

    Negative or invalid slice values did not work as expected.
    Added more test cases for Arrays.
    
    Use '._move_slice' instead of '.slice'.

--- test/parse-slices.js
+++ test/parse-slices.js
@@ -6,20 +6,20 @@ var res, compilerOptions = {raw: 1};
 res = move.compile('x = foo[1]', compilerOptions);
 assert.notEqual(res.indexOf('x = foo[1]'), -1);
 
-res = move.compile('x = foo.slice(1, 2)', compilerOptions);
-assert.notEqual(res.indexOf('x = foo.slice(1, 2)'), -1);
+res = move.compile('x = foo._move_slice(1, 2)', compilerOptions);
+assert.notEqual(res.indexOf('x = foo._move_slice(1, 2)'), -1);
 
 res = move.compile('x = foo[1:2]', compilerOptions);
-assert.notEqual(res.indexOf('x = foo.slice(1, 2)'), -1);
+assert.notEqual(res.indexOf('x = foo._move_slice(1, 2)'), -1);
 
 res = move.compile('x = foo[1:]', compilerOptions);
-assert.notEqual(res.indexOf('x = foo.slice(1)'), -1);
+assert.notEqual(res.indexOf('x = foo._move_slice(1)'), -1);
 
 res = move.compile('x = foo[:2]', compilerOptions);
-assert.notEqual(res.indexOf('x = foo.slice(0, 2)'), -1);
+assert.notEqual(res.indexOf('x = foo._move_slice(0, 2)'), -1);
 
 res = move.compile('x = foo[:]', compilerOptions);
-assert.notEqual(res.indexOf('x = foo.slice(0)'), -1);
+assert.notEqual(res.indexOf('x = foo._move_slice(0)'), -1);
 
 // slice-write
 res = move.compile('foo[1:2] = [1,2,3]\nx = 5', compilerOptions);
