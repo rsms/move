cd72d286a8af6a632df85b4aecb9524999e200af
Rasmus Andersson <rsms@fb.com>
2011-06-19 16:53:00 -0700

    gh pages

--- web/build.sh
+++ web/build.sh
@@ -3,15 +3,16 @@ SRC_DIR="$(perl -e "use Cwd; use Cwd 'realpath'; print realpath('${0}');" | xarg
 DST_DIR="$(perl -e "use Cwd; use Cwd 'realpath'; print realpath('${SRC_DIR}');" | xargs dirname)"
 BUILD_DST_DIR="${SRC_DIR}/.build"
 DST_DIR="${DST_DIR}/web_generated"
+GIT_SRC_DIR="$(dirname "$SRC_DIR")"
 
-echo "Source: ${SRC_DIR}"
+echo "Building from ${SRC_DIR} -> ${DST_DIR}"
 
 MOVE_BIN="${SRC_DIR}/../bin/move"
 
 cd "$SRC_DIR" || exit $?
 
 # Check if DST_DIR is ok
-if [ ! -d "$DST_DIR" ] || [ "$(git --work-tree="${DST_DIR}" branch --no-color | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1/")" != "gh-pages" ]; then
+if [ ! -d "$DST_DIR" ] || [ "$(git --git-dir="${DST_DIR}/.git" branch --no-color | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1/")" != "gh-pages" ]; then
   # Backup if modified
   if [ -d "$DST_DIR" ]; then
     BACKUP_DIR="${DST_DIR}_backup_"$(date +%s)
@@ -21,7 +22,9 @@ if [ ! -d "$DST_DIR" ] || [ "$(git --work-tree="${DST_DIR}" branch --no-color | 
   fi
 
   # Clone
-  git clone --local --shared --no-checkout -- .. "$DST_DIR" || exit $?
+  git clone --local --shared --no-checkout -- \
+    "$GIT_SRC_DIR" "$DST_DIR" || exit $?
+
   cd "$DST_DIR" || exit $?
 
   # Create or checkout gh-pages branch
@@ -35,27 +38,29 @@ if [ ! -d "$DST_DIR" ] || [ "$(git --work-tree="${DST_DIR}" branch --no-color | 
 
   # Back home
   cd "$SRC_DIR" || exit $?
-else
-  echo "Destination: ${DST_DIR}"
 fi
 
 # Build move
 "$MOVE_BIN" ../browser/build.mv || exit $?
 
 # Build website
+rm -rf "$BUILD_DST_DIR"
 jekyll --no-server --no-auto "$BUILD_DST_DIR" || exit $?
 mv "$DST_DIR/.git" "$BUILD_DST_DIR/.git" || exit $?
-mv -f "$BUILD_DST_DIR" "$DST_DIR"
+rm -rf "$DST_DIR"
+mv -f "$BUILD_DST_DIR" "$DST_DIR" || exit $?
+
+# Create .nojekyll
+touch "$DST_DIR/.nojekyll"
 
 # Commit
 cd "$DST_DIR" || exit $?
 git add . || exit $?
 git commit -a -m 'Generated website' || exit $?
-git remote -v
-#git push origin gh-pages || exit $?
+git remote set-url origin "$GIT_SRC_DIR" || exit $?
+git push origin gh-pages || exit $?
 
 # Back home
 cd "$SRC_DIR" || exit $?
 
-echo "$DST_DIR"
-
+echo "Done. Deploy with: git push origin gh-pages"
